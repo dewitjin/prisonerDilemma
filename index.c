0f133f91e9dbc3dd45d17f395a8adff238754aef
Dewi Tjin <dewi.tjin@gmail.com>
2017-03-20 01:53:11 -0700

    game mark up and comments without the results

---
+++ index.c
@@ -0,0 +1,61 @@
+//Code TODO:
+
+//display a count of how many people are playing the game
+
+//explain the scenario
+
+//prompt for a friend's name and email you would like to play with
+//prompt for user to defect or keep silence
+//email friend link to github to download a new game and ask friend for her respond,
+//ensure friend’s response will be email back to PD email, so I can keep track, or just to email back user and that’s it
+
+
+//show user result of gave after emailing friend and prompting for user response - let’s wait for what your friend will say, 
+//give a success message
+//let’s hope your friend will keep silence, if not then the both of you will get this…
+
+/**
+
+(this is to be prompt to console)
+
+Welcome to one of the most interesting game ever. Let’s get started:
+
+Unfortunately, the popo’s are onto you.  The popo have you and your friend in 2 separate cells and are accusing both of you for a crime.  You both did not commit the crime.  You and your friend are not able to talk with each other.  The crime is punishable by 30 years in jail.  
+
+Now the prosecutor comes to talk to you.  She tells you she has enough to convict both of you for a lesser crime, punishable by one year in jail.  
+
+Hang on!  
+
+The prosecutor offers you and your friend the same deal though.  This is the deal:  
+If you confess and screw your friend (and your friend remains quiet), you will go free and your friend will have to go to jail for 30 years.  If you screw your friend and your friend screws you too, however, both of you will have to go to jail for 15 years.
+
+Urban dictionary: POPO - A Police officer. especially the ones that rides on bikes.
+**/
+
+//prompt
+
+//What are you going to do?
+
+//Please enter a friend’s name you want to play with:
+
+//Please enter their email address so I can ask them for their response:
+
+/**
+
+Email content.
+
+Hi.  I am play a game and I want you to play along.  All I need to do is email me back with one of two options.  Are you going to screw me or not?  
+
+Thanks.
+**/
+
+//prompt
+//Please enter what you are going to do.  Click 1 to SCREW YOUR FRIEND, or 2 to REMAIN SILENT.
+
+//after email is sent shows the result
+
+//The email was sent successfully.
+
+//printf point of the PD game
+
+
